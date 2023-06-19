b2f1735a625ea3bfb2486425ec636c7a1fe6a689
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2023-06-18 17:11:50 -0700

    removed unnessisary examples

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,19 +8,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// test funcc
-app.get('/patients', async (_, res) => {
-  try {
-    const snapshot = await db.collection('patients').get();
-    const patients = snapshot.docs.map((doc) => doc.data());
-    res.json(patients);
-  } catch (err) {
-    if (err instanceof Error) {
-      res.status(500).send(err.message);
-    }
-  }
-});
-
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
